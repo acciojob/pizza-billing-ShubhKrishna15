f809c797b954a26fc2b80a589255e72a7e12ed59
ShubhKrishna15 <Krsnavasudey@gmail.com>
2023-01-18 14:52:44 +0530

    Solution-Pizza1

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -3,7 +3,14 @@ package com.driver;
 public class DeluxePizza extends Pizza {
 
     public DeluxePizza(Boolean isVeg) {
-        super(isVeg);
-        // your code goes here
+       super(isVeg);
+       addExtraCheese();
+       addExtraToppings();
+
+
     }
+
+
 }
+
+
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -4,11 +4,18 @@ public class Pizza {
 
     private int price;
     private Boolean isVeg;
+    private Boolean cheeze=false;
+    private Boolean topping=false;
+    private int takeAway = 0 ;
     private String bill;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+        if(isVeg){
+            this.price = 300;
+        }else{
+            this.price = 400;
+        }
     }
 
     public int getPrice(){
@@ -16,19 +23,54 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+        if(!cheeze){
+            this.price+=80;
+            cheeze=true;
+        }
+
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(!topping){
+            if(isVeg){
+                this.price += 70;
+            }
+            else{
+                this.price+=120;
+            }
+            topping = true;
+        }
+
     }
 
     public void addTakeaway(){
-        // your code goes here
+
+        price +=20;
+        takeAway+= 20;
+
     }
 
     public String getBill(){
-        // your code goes here
+        if(isVeg){
+            this.bill = "Base Price Of The Pizza: 300"+"\n";
+
+        }else{
+            this.bill ="Base Price Of The Pizza: 400"+"\n";
+        }
+        if(cheeze){
+            this.bill += "Extra Cheese Added: 80" +"\n";
+        }
+        if(topping){
+            if(isVeg){
+            this.bill += "Extra Toppings Added: 70"+"\n" ; }
+            else{
+                this.bill += "Extra Toppings Added: 120" + "\n";
+            }
+        }
+        if(takeAway>0){
+            this.bill += "Paperbag Added: "+ Integer.toString(takeAway)+"\n";
+        }
+        this.bill += "Total Price: " + Integer.toString(price) + "\n";
         return this.bill;
     }
 }
